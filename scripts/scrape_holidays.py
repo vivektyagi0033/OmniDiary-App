45ab9cb53eebe402a6a9a72c5480535cce95691f
vivektyagi0033 <vivektyagi0033@gmail.com>
2025-07-20 14:07:21 +0530

    Update scrape_holidays.py

--- scripts/scrape_holidays.py
+++ scripts/scrape_holidays.py
@@ -1,1 +1,7 @@
+import requests
+import json
 
+# Fetches public holidays for 2024
+response = requests.get("https://date.nager.at/api/v3/PublicHolidays/2024/US")
+with open("events/holidays.json", "w") as f:
+    json.dump(response.json(), f)
